da90f45adc3b43450f78c1c602b9a7fdb598ebcc
sridharan01234 <sridharan01234@gmail.com>
2024-04-17 18:06:26 +0530

    desctructor implemented

---
+++ Controller/DestructorController.php
@@ -0,0 +1,7 @@
+<?php
+require "BaseController.php";
+class DestructorController extends BaseController {
+    public function view() {
+        require "./View/DestructorClass.php";
+    }
+}
---
+++ View/DestructorClass.php
@@ -0,0 +1,14 @@
+<?php
+
+class DestructorClass {
+    public function __construct() {
+        echo "Hi Iam from constructor<br>";
+    }
+
+    public function __destruct() {
+        echo "Hi I am Destroyed";
+    }
+}
+
+$init = new DestructorClass();
+unset($init);
