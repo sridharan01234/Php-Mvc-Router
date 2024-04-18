11ad22f3b168f558183e6b6620e116078dffd523
sridharan01234 <sridharan01234@gmail.com>
2024-04-18 09:29:22 +0530

    product controller added

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -2,16 +2,29 @@
 require "BaseController.php";
 class ProductController extends BaseController
 {
+    /**
+     * It Displays products view file
+     *
+     * @return void
+     */
     public function products(): void
     {
         require "View/products.php";
     }
-
+    /**
+     * It Displays productsList view file
+     *
+     * @return void
+     */
     public function productsList(): void
     {
         require "View/productsList.php";
     }
-
+    /**
+     * It Displays view file
+     *
+     * @return void
+     */
     public function view(): void
     {
         require "View/products.php";
