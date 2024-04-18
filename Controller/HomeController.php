af427692603440478085bc9b414395e72e16b80f
sridharan01234 <sridharan01234@gmail.com>
2024-04-18 09:29:11 +0530

    Home controller added

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -3,15 +3,31 @@ require "BaseController.php";
 
 class HomeController extends BaseController
 {
+    /**
+     * It Displays index view file
+     *
+     * @return void
+     */
     public function index(): void
     {
         require "View/index.php";
     }
+    /**
+     * It Displays home view file
+     *
+     * @return void
+     */
     public function home(): void
     {
         require "View/home.php";
     }
-    public function view (): void {
+    /**
+     * It Displays view file
+     *
+     * @return void
+     */
+    public function view(): void
+    {
         require "View/index.php";
     }
 }
