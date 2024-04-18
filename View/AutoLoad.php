78763d1b3af4a0b69a31be4f13e178b78b338ef8
sridharan01234 <sridharan01234@gmail.com>
2024-04-18 09:30:18 +0530

    Autoload implemented

--- View/AutoLoad.php
+++ View/AutoLoad.php
@@ -1,7 +1,7 @@
-<?php 
-	spl_autoload_register(function ($class_name) { 
-	include $class_name . '.php'; 
-	}); 
-	$obj = new HomeController(); 
-	$obj2 = new StaticClass(); 
-	echo "objects of HomeController and Static class created successfully";
+<?php
+spl_autoload_register(function ($class_name) {
+    include $class_name . '.php';
+});
+$obj = new HomeController();
+$obj2 = new StaticClass();
+echo "objects of HomeController and Static class created successfully";
